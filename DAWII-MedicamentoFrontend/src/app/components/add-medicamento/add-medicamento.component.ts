db2fa054fc155705cec8fa3e3ed8ea7db8349932
Slackna <82481408+Slackna@users.noreply.github.com>
2021-09-24 19:07:05 -0500

    FrontendFinalizado

---
+++ DAWII-MedicamentoFrontend/src/app/components/add-medicamento/add-medicamento.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { MedicamentoService } from 'src/app/services/medicamento.service';
+import { Medicamento} from 'src/app/models/medicamento.model';
+@Component({
+  selector: 'app-add-medicamento',
+  templateUrl: './add-medicamento.component.html',
+  styleUrls: ['./add-medicamento.component.css']
+})
+export class AddMedicamentoComponent implements OnInit {
+
+  constructor(private medicamentoService: MedicamentoService) { }
+
+  medicamento:Medicamento = {
+
+   } 
+
+
+
+
+  saveMedicamento(){
+    console.log(">> saveMedicamento");
+   console.log(this.medicamento);
+    this.medicamentoService.create(this.medicamento).subscribe(
+      response => {
+        console.log(response.mensaje);
+        alert(response.mensaje)
+
+       
+      },
+     error => { console.log(error);}
+
+    );
+   
+}
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ DAWII-MedicamentoFrontend/src/app/models/medicamento.model.ts
@@ -0,0 +1,14 @@
+export class Medicamento {
+   
+    idMedicammento?: number;
+
+    nombre?: string;
+
+    precio?: number;
+
+    stock?: number;
+
+    laboratorio?: string;;
+
+
+}
